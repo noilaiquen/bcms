8c0e8d2568d316fce2323ec320261e5a1b061d77
Nguyễn Thanh Bình <nguyenthanhbinh@Nguyens-MacBook-Pro.local>
2018-07-31 14:19:21 +0700

    add site

--- application/modules/admincp_modules/views/ajax_loadContent.php
+++ application/modules/admincp_modules/views/ajax_loadContent.php
@@ -4,6 +4,7 @@
             <th class="center">No.</th>
             <th class="center"><input type="checkbox" id="selectAllItems" onclick="selectAllItems(<?=count($results)?>)"></th>
             <th class="sorting" onclick="sort('name')" id="name">Name</th>
+            <th class="center sorting" onclick="sort('site')" id="site">Site</th>
             <th class="center sorting" onclick="sort('status')" id="status">Status</th>
             <th class="center sorting" onclick="sort('created')" id="created">Created</th>
         </tr>
@@ -16,6 +17,7 @@
                     <td class="num_row center"><?=$key + 1?></td>
                     <td class="num_row center"><input type="checkbox" id="item<?=$i?>" onclick="selectItem(<?=$i?>)" value="<?=$result['id']?>"></td>
                     <td><a href="javascript:void(0)"><?=$result['name']?></a></td>
+                    <td class="center"><?=$result['site']?></td>
                     <td class="center" style="width: 150px" id="updateStatus<?=$result['id']?>">
                         <a href="javascript:void(0)" onclick="updateStatus('<?=$result['id']?>', '<?=$result['status']?>')">
                             <span class="badge bg-<?=$result['status']==1?'success':'danger';?>"><?=$result['status']==1?'Active':'Disable';?></span>
